09e05802918a2e69539a2e8648185cb70f338231
Jou Okuyama <ojoxux@gmail.com>
2024-10-24 19:26:41 +0900

    chore: 🛠️ ルールとグローバルの追加によるESLint設定の更新

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   { ignores: ['dist'] },
@@ -10,14 +10,19 @@ export default [
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node, // Node.js環境のグローバル変数を追加
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
     },
-    settings: { react: { version: '18.3' } },
+    settings: {
+      react: { version: '18.3' },
+    },
     plugins: {
       react,
       'react-hooks': reactHooks,
@@ -33,6 +38,17 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      // 追加のルール設定
+      'react/prop-types': 'off', // PropTypesチェックを無効化
+      'no-unused-vars': [
+        'warn',
+        {
+          // 未使用変数の警告
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      'no-console': 'warn', // console.log()の使用を警告
     },
   },
-]
+];
