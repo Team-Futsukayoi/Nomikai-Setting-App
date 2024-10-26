7d564a774ba32612dbbca2809ab37acad54e9c26
Jou Okuyama <ojoxux@gmail.com>
2024-10-27 01:58:41 +0900

    fix: snackbarをコンポーネント分割し、再利用しやすいように変更

---
+++ src/components/SnackbarComponent.jsx
@@ -0,0 +1,34 @@
+import { Snackbar, Alert } from '@mui/material';
+
+const SnackbarComponent = ({ open, message, handleClose }) => {
+  return (
+    <Snackbar
+      open={open}
+      autoHideDuration={3000}
+      onClose={handleClose}
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      sx={{
+        position: 'fixed',
+        bottom: '67px',
+        zIndex: 1100,
+      }}
+    >
+      <Alert
+        onClose={handleClose}
+        severity="success"
+        sx={{
+          width: '100%',
+          backgroundColor: '#FFD700',
+          color: '#000',
+          '& .MuiAlert-icon': {
+            color: '#000',
+          },
+        }}
+      >
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+};
+
+export default SnackbarComponent;
