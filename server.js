06f51c90b496c8aab53704a1dd292e8a9f004601
yt-ys <yt-ys>
2024-10-26 13:03:09 +0900

    feat: add root endpoint to server.js

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+// ルートエンドポイントのハンドラー
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
 // サーバーを起動
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-  });
-  
+  console.log(`Server is running at http://localhost:${port}`);
+});
