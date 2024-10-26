b199c20f90c372bb74b533e1ae2ab544f246f861
Jou Okuyama <ojoxux@gmail.com>
2024-10-27 07:39:54 +0900

    feat: snackbarの位置、消える時間を呼び出し元で編集できるように変更

--- src/components/SnackbarComponent.jsx
+++ src/components/SnackbarComponent.jsx
@@ -1,15 +1,21 @@
 import { Snackbar, Alert } from '@mui/material';
 
-const SnackbarComponent = ({ open, message, handleClose }) => {
+const SnackbarComponent = ({
+  open,
+  message,
+  handleClose,
+  bottomOffset,
+  autoHideDuration = 1500,
+}) => {
   return (
     <Snackbar
       open={open}
-      autoHideDuration={3000}
+      autoHideDuration={autoHideDuration}
       onClose={handleClose}
       anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
       sx={{
         position: 'fixed',
-        bottom: '90px',
+        bottom: bottomOffset,
         zIndex: 1100,
       }}
     >
