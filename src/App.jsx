7bdbda575f8fa61d006ff4c8c8a7b14a3858caa7
Jou Okuyama <ojoxux@gmail.com>
2024-10-27 14:12:33 +0900

    feat: add UserAttributesPage component for user profile attributes

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,37 @@ import ChatPage from './pages/chatpage/ChatPage';
 import SuccessPage from './pages/SuccessPage';
 import Header from './components/Header';
 import NavigationBar from './components/NavigationBar';
-import { useLocation } from 'react-router-dom/dist';
+import { useLocation } from 'react-router-dom';
 import ProfilePage from './pages/user/ProfilePage';
+import UserAttributesPage from './pages/user/UserAttributesPage';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ja';
+
+// 日本語ロケールを設定
+dayjs.locale('ja');
 
 function App() {
   const location = useLocation();
-  const hideNavBarPaths = ['/', '/signin', '/signup'];
+  const hideNavBarPaths = ['/', '/signin', '/signup', '/user-attributes'];
 
   return (
     <>
-      {!hideNavBarPaths.includes(location.pathname) && <Header />}
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/success" element={<SuccessPage />} />
-        <Route path="/chatlist" element={<ChatListPage />} />
-        <Route path="/chat/:friendId" element={<ChatPage />} />
-      </Routes>
-      {!hideNavBarPaths.includes(location.pathname) && <NavigationBar />}
+      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ja">
+        {!hideNavBarPaths.includes(location.pathname) && <Header />}
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/success" element={<SuccessPage />} />
+          <Route path="/chatlist" element={<ChatListPage />} />
+          <Route path="/chat/:friendId" element={<ChatPage />} />
+          <Route path="/user-attributes" element={<UserAttributesPage />} />
+        </Routes>
+        {!hideNavBarPaths.includes(location.pathname) && <NavigationBar />}
+      </LocalizationProvider>
     </>
   );
 }
