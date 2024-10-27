49ec2bfdce2e9857e5b862baf6152bf7f89288cf
Jou Okuyama <ojoxux@gmail.com>
2024-10-27 14:13:04 +0900

    feat: add alcohol strength options to constants.js

---
+++ src/consts/constants.js
@@ -0,0 +1,6 @@
+export const alcoholStrengthOptions = [
+  { value: 'strong', label: '強い', icon: '🍺' },
+  { value: 'medium', label: '普通', icon: '🍷' },
+  { value: 'weak', label: '弱い', icon: '🥂' },
+  { value: 'none', label: '飲まない', icon: '🚫' },
+];
